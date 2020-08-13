2f28413fcbc8c92755a1e06a8ccd4b4f4acd2a01
Bongani Mbigi <bongani@utexas.edu>
2020-08-13 02:38:51 -0500

    real db fix

--- src/main/java/hello/Person.java
+++ src/main/java/hello/Person.java
@@ -11,7 +11,7 @@ public class Person {
 
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
 
     private String name;
