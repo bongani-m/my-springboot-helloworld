a64e020f816d74b74dcdebf7771439623e1e7866
Bongani Mbigi <bongani@utexas.edu>
2020-08-13 04:40:52 -0500

    ffff

--- src/main/java/hello/Person.java
+++ src/main/java/hello/Person.java
@@ -1,12 +1,11 @@
 package hello;
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+
+import javax.persistence.*;
 
 @Entity
+@Table(name = "person")
 public class Person {
 
 
